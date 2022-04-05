b06c5571fd6e35f5b2bcc0f88a03b93685ae28b9
Linus <linus.123@hotmail.com>
2022-04-05 11:22:09 +0200

    test

--- app/frontend/LeggTilBevisKnapp.jsx
+++ app/frontend/LeggTilBevisKnapp.jsx
@@ -5,7 +5,7 @@ const LeggTilBevisKnapp = ({ history }) => (
   <div className="blokk">
     <div className="legg-til-bevis-knapp knapp" onClick={() => history.push('/nytt-bevis')}>
       <i className="fa fa-plus" aria-hidden="true"></i>
-      <span>Legg til nytt bevis</span>
+      <span>LINUS</span>
     </div>
   </div>
 );
